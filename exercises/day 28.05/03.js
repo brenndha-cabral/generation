917de62afa62e75d0a0324d6b4aa919e472b834e
brenndha-cabral <cabralbrenndha@gmail.com>
2025-05-28 14:39:28 -0300

    feat: exercise 3

---
+++ exercises/day 28.05/03.js
@@ -0,0 +1,13 @@
+const read = require("readline-sync");
+
+let age = 0, youngAge = 0, oldAge = 0;
+
+while (true) {
+  age = read.questionInt("Digite uma idade: ");
+
+  if (age < 0) break;
+  if (age < 21) youngAge++
+  if (age > 50) oldAge++
+}
+
+console.log(`Total de pessoas menores de 21 anos: ${youngAge} | Total de pessoas maiores de 50 anos: ${oldAge}`);
